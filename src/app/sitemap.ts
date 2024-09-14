03305ba338928d122c6970828bcb92ac635cc935
Daniel Hilmer <daniel.hilmer@outlook.de>
2024-09-14 17:37:17 +0200

    refactor: Update sitemap priorities and add new URLs
    feat: Update tailwind configuration

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,19 +6,37 @@ export default function sitemap(): MetadataRoute.Sitemap {
             url: 'https://dev.danielhilmer.de',
             lastModified: new Date(),
             changeFrequency: 'yearly',
-            priority: 1,
-        },
-        {
-            url: 'https://acme.com/about',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'https://acme.com/blog',
+            url: 'https://dev.danielhilmer.de/projekte/dev',
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
+            changeFrequency: 'yearly',
+            priority: 0.7,
+        },
+        {
+            url: 'https://dev.danielhilmer.de/projekte/dev/window-effect-header',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.6,
+        },
+        {
+            url: 'https://dev.danielhilmer.de/projekte/kunden',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.7,
+        },
+        {
+            url: 'https://dev.danielhilmer.de/custom',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: 'https://dev.danielhilmer.de/wordpress',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
         },
     ]
 }
